468cb056455779f0ec2777a837ba00c0b7ffcaf7
Rostrv <trovatorosario91@gmail.com>
2022-06-24 21:23:15 +0200

    added relations

---
+++ database/seeds/TagSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Models\Tag;
+use Illuminate\Support\Str;
+
+class TagSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = ['coding', 'laravel', 'css', 'js', 'vue', 'sql'];
+        foreach ($tags as $tag) {
+            $new_tag = new Tag();
+            $new_tag->name = $tag;
+            $new_tag->slug = Str::slug($new_tag->name);
+            $new_tag->save();
+        }
+    }
+}
--- resources/views/admin/Posts/show.blade.php
+++ resources/views/admin/Posts/show.blade.php
@@ -4,4 +4,17 @@
 <img src="{{$post->cover_image}}" alt="{{$post->title}}">
 <h1>{{$post->title}}</h1>
 <div class="content">{{$post->content}}</div>
+<div>Category: {{ $post->category ? $post->category->name : 'Uncategorized'}}</div>
+<div class="tags">
+    tags:
+    @if(count($post->tags) > 0) 
+        @foreach($post->tags as $tag)
+            {{$tag->name}}
+        @endforeach
+    @else 
+        <span>No tags</span>
+
+    @endif
+
+</div>
 @endsection
